8674f7470d80bac92611df3b5b1692386926b9a4
Dmitry Strokov <dmitry.strokov@mail.ru>
2019-03-26 17:48:01 +0500

    сделал 5 лабу

--- app.py
+++ app.py
@@ -2,6 +2,7 @@
 from flask import Flask, jsonify
 from lab1.lab1 import lab1
 from lab4.lab4 import lab4
+from lab5.lab5 import lab5
 
 app = Flask(__name__)
 from lab2.lab2 import lab2
@@ -9,6 +10,7 @@ from lab2.lab2 import lab2
 app.register_blueprint(lab1, url_prefix='/lab1')
 app.register_blueprint(lab2, url_prefix='/lab2')
 app.register_blueprint(lab4, url_prefix='/lab4')
+app.register_blueprint(lab5, url_prefix='/lab5')
 
 @app.errorhandler(404)
 def not_found(error):
